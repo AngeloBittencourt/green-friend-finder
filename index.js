07fe2744202b88f4b5443d9364013d5f0b021c6d
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-11-07 07:20:38 -0300

    criando container das plantas

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const noSunIcon = require('./images/icons/no-sun.svg');
+const lowSunIcon = require('./images/icons/low-sun.svg');
+const oneDropIcon = require('./images/icons/1-drop.svg');
+const twoDropIcon = require('./images/icons/2-drops.svg');
+const threeDropIcon = require('./images/icons/3-drops.svg');
+const petIcon = require('./images/icons/pet.svg');
+const toxicIcon = require('./images/icons/toxic.svg');
+
 let plantList = [];
 
 function fetchFromAPi(sun, water, havePet) {
@@ -18,6 +26,7 @@ window.onload = () => {
     const petValue = petSelect.value;
     if(sunValue !== '' && waterValue !== '' && petValue !== '') {
       fetchFromAPi(sunValue, waterValue, petValue);
+      plantList.join(' ')
       createPlantsElement();
     }
   }
@@ -26,17 +35,46 @@ window.onload = () => {
   waterSelect.addEventListener('change',onSelect);
   petSelect.addEventListener('change', onSelect);
 
+  function sunsIcons(sunStatus) {
+    if(sunStatus === 'low'){
+      return lowSunIcon;
+    } if(sunStatus === 'no') {
+      return noSunIcon;
+    }
+  }
+
+  function waterIcon(waterStatus) {
+    if(waterStatus === 'rarely'){
+      return oneDropIcon;
+    } if(waterStatus === 'daily') {
+      return twoDropIcon;
+    } else{
+      return threeDropIcon;
+    }
+  }
+
   function createPlants () {
+
     return  `<div class="plants">
     ${plantList.map((plant) => {
       return `
       <div class="plant">
-        <h1>${plant.name}</h1>
-        
+        ${plant.staff_favorite ? `<div> Staff favorite </div>` : ``}
+        <div>
+          <img src="${plant.url}" alt="${plant.name}">
+          <h3>${plant.name}</h3>
+          <p>$${plant.price}</p>
+        </div>
+        <div>
+          <img src="${plant.toxicity ? petIcon : toxicIcon}" alt="${plant.toxicity}">
+          <img src="${sunsIcons(plant.sun)}" alt="${plant.sun}">
+          <img src="${waterIcon(plant.water)}" alt="${plant.water}">
+        </div>
       </div>`
-    })}
+    }).join(' ')}
     </div>`
   }
+
 
   function createPlantsElement() {
     const plants = document.getElementById('plants-results');
@@ -46,5 +84,5 @@ window.onload = () => {
     }
   }
 
-  
-}  
+}
+
